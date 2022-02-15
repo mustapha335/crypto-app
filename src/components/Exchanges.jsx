0b0dffd6babc476aa971b98895145e16c11d9539
mustapha335 <reezy77jay@gmail.com>
2022-02-15 22:50:26 +0000

    first commit

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
+import React from 'react'
+import millify from 'millify'
+import { Collapse, Row, Col, Typography, Avatar } from 'antd'
+import HTMLReactParser from 'html-react-parser'
 
-import { useGetExchangesQuery } from '../services/cryptoApi';
-import Loader from './Loader';
+import { useGetExchangesQuery } from '../services/cryptoApi'
+import Loader from './Loader'
 
-const { Text } = Typography;
-const { Panel } = Collapse;
+const { Text } = Typography
+const { Panel } = Collapse
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
-  const exchangesList = data?.data?.exchanges;
- // Note: To access this endpoint you need premium plan
-  if (isFetching) return <Loader />;
+  const { data, isFetching } = useGetExchangesQuery()
+  const exchangesList = data?.data?.exchanges
+  // Note: To access this endpoint you need premium plan
+  if (isFetching) return <Loader />
 
   return (
     <>
@@ -50,7 +50,7 @@ const Exchanges = () => {
         ))} */}
       </Row>
     </>
-  );
-};
+  )
+}
 
-export default Exchanges;
+export default Exchanges
